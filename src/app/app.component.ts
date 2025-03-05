84ef129fc279a08e58a3d62e84faaca6fcc7c1b4
naike.drame-codelab <naike.drame.dev@gmail.com>
2025-03-05 01:10:40 +0100

    till form lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { HomeComponent } from "./home/home.component";
-
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
-  <main>
-    <header class="brand-name">
-    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-    </header>
-  </main>
-  <section class="content">
-    <app-home></app-home>
-  </section>
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent],
 })
 export class AppComponent {
   title = 'homes';
